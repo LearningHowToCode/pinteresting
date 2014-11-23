fcebd6b491a55981be134f42fb028263f20e3531
Nobu Iguchi <nobuhito.iguchi@gmail.com>
2014-11-23 12:09:47 -0500

    Added Amazon S3 for Paperclip uploads to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Pinteresting::Application.configure do
 
   #Required for Heroku
   # Note to set it to actual host
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'nobu-pinteresting.herokuapp.com' }
 
   config.paperclip_defaults = {
     :storage => :s3,
